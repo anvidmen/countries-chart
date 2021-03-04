db6566e45ba1e09e22d1b863b52db939eb556b37
Juan David Om <anvidmen@gmail.com>
2021-03-04 21:09:02 +0100

    Add unit component of Input

---
+++ src/components/units/Input/Input.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { StyledError, StyledInput } from './styles'
+
+const Input = ({
+  type, placeholder, ref, value, onChange, onFocus, onBlur,
+  className, id, name, minlength, errorText, error, disabled
+}) => {
+  return (
+    <div>
+      <StyledInput
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        ref={ref}
+        className={`${className} ${error ? 'error' : ''}`}
+        id={id}
+        name={name}
+        disabled={disabled}
+        minLength={minlength}
+      />
+      <StyledError
+        dangerouslySetInnerHTML={{ __html: error ? errorText : null }}
+        className={className}
+      />
+    </div>
+  )
+}
+
+export default Input

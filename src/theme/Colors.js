872d008ee820ffb79c2dfe8307df81e05d5cc27e
Juan David Om <anvidmen@gmail.com>
2021-03-04 20:56:06 +0100

    Add Body component

---
+++ src/theme/Colors.js
@@ -0,0 +1,15 @@
+const Colors = {
+  bgColor: 'rgb(0, 0, 0)',
+  fontColor: '#ffff',
+  bgCard: 'rgba(205, 205, 205, 0.2)',
+  bgDisabledCard: 'rgba(205, 205, 205, 0.9)',
+  hoverNav: '#09dbdb',
+  bgHead: 'rgb(8, 166, 166)',
+  redColor: 'rgba(193, 12, 12, 1)',
+  lightBlue: '#09dbdb',
+  darkBlue: '#229c9c',
+  lightOrange: '#F76546',
+  darkOrange: '#E62900'
+}
+
+export default Colors

d02ca25f7ea3ae0fa547919d8dac194ccb2a60ee
Juan David Om <anvidmen@gmail.com>
2021-03-04 21:35:42 +0100

    Add localStorage

---
+++ src/utils/storage.js
@@ -0,0 +1,13 @@
+
+export const refresh = () => {
+  window.location.reload()
+}
+
+export const auth = user => {
+  return localStorage.setItem('user', JSON.stringify(user))
+}
+
+export const logout = () => {
+  localStorage.clear()
+  refresh()
+}

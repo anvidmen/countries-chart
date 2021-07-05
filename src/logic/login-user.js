05055f2d58027e1f51dae665471445c80c003292
anvidmen <anvidmen@gmail.com>
2021-07-05 18:13:18 +0200

    remove headers setting

--- src/logic/login-user.js
+++ src/logic/login-user.js
@@ -1,17 +1,10 @@
 import { login } from 'api'
-import qs from 'qs'
 import { auth } from 'utils/storage'
 
-const config = {
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded; charset=utf-8'
-  }
-}
-
 const loginUser = async (username, password) => {
-  const data = qs.stringify({ username: username, password: password })
+  const data = { username: username, password: password }
   try {
-    const response = await login(data, config)
+    const response = await login(data)
 
     const { data: { message } } = response
     const { user_data: user } = message

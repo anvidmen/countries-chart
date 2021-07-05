a49c96b9ecdff198cfa70c817d008a61bd9fe026
anvidmen <anvidmen@gmail.com>
2021-07-05 19:05:06 +0200

    fix url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:5000'
+const baseURL = 'http://localhost:5000/'
 
 export const login = data => axios.get(`${baseURL}login`, data)
 export const getCountries = () => axios.get(`${baseURL}countries`)
